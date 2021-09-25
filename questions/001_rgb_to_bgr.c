1bec9a484b8db24d7aee690d6a06048a977d7704
kentakuramochi <soratobi93@gmail.com>
2021-09-25 10:32:54 +0900

    add q1

---
+++ questions/001_rgb_to_bgr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "imgdata.h"
+
+Imgdata *rgb2bgr(Imgdata *img)
+{
+    Imgdata *bgr = Imgdata_alloc(img->width, img->height, img->channel);
+
+    for (int y = 0; y < img->height; y++)
+    {
+        for (int x = 0; x < img->width; x++)
+        {
+            uint8_t r = Imgdata_at(img, x, y)[0];
+            uint8_t g = Imgdata_at(img, x, y)[1];
+            uint8_t b = Imgdata_at(img, x, y)[2];
+
+            Imgdata_at(bgr, x, y)[0] = b;
+            Imgdata_at(bgr, x, y)[1] = g;
+            Imgdata_at(bgr, x, y)[2] = r;
+        }
+    }
+
+    return bgr;
+}
+
+int main(int argc, char *argv[])
+{
+    Imgdata *img = Imgdata_read_png(argv[1]);
+
+    Imgdata *img_bgr = rgb2bgr(img);
+
+    Imgdata_write_png(img_bgr, "./001_bgr.png");
+
+    Imgdata_free(img);
+    Imgdata_free(img_bgr);
+
+    return 0;
+}
