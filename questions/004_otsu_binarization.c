abdb7ca50b8d6abd30c88bc36f29b78b30562a2a
mts0629 <soratobi93@gmail.com>
2022-02-22 03:54:00 +0900

    q4

--- questions/004_otsu_binarization.c
+++ questions/004_otsu_binarization.c
@@ -4,14 +4,40 @@
 
 Imgdata *binarize_otsu(Imgdata *img)
 {
-    Imgdata *bin = Imgdata_alloc(img->width, img->height, 1);
+    // get grayscale
+    Imgdata *gray = Imgdata_alloc(img->width, img->height, 1);
+    uint8_t min = 255;
+    uint8_t max = 0;
+    for (int y = 0; y < img->height; y++)
+    {
+        for (int x = 0; x < img->width; x++)
+       {
+            uint8_t r = Imgdata_at(img, x, y)[0];
+            uint8_t g = Imgdata_at(img, x, y)[1];
+            uint8_t b = Imgdata_at(img, x, y)[2];
+            uint8_t i = (uint8_t)(0.2126 * r + 0.7152 * g + 0.0722 * b);
+
+            if (i < min)
+            {
+                min = i;
+            }
+            if (i > max)
+            {
+                max = i;
+            }
+
+            Imgdata_at(gray, x, y)[0] = i;
+        }
+    }
+
+    const int size = gray->width * gray->height;
 
     // max inter-class variance
     long double max_icvar = -1;
 
-    // decide best threshold
-    int best_th = 0;
-    for (int i = 1; i < 255; i++)
+    // search best threshold
+    uint8_t best_th = 0;
+    for (int i = min; i < max; i++)
     {
         int th = i;
 
@@ -23,41 +49,31 @@ Imgdata *binarize_otsu(Imgdata *img)
         double m1 = 0;
 
         // get num of pix and mean of class 0/1
-        for (int y = 0; y < img->height; y++)
+        for (int j = 0; j < size; j++)
         {
-            for (int x = 0; x < img->width; x++)
+            uint8_t p = gray->data[j];
+            if (p <= th)
             {
-                uint8_t r = Imgdata_at(img, x, y)[0];
-                uint8_t g = Imgdata_at(img, x, y)[1];
-                uint8_t b = Imgdata_at(img, x, y)[2];
-                uint8_t gray = (uint8_t)(0.2126 * r + 0.7152 * g + 0.0722 * b);
-
-                if (gray <= th)
-                {
-                    w0++;
-                    m0 += gray;
-                }
-                else
-                {
-                    w1++;
-                    m1 += gray;
-                }
+                w0++;
+                m0 += p;
+            }
+            else
+            {
+                w1++;
+                m1 += p;
             }
         }
 
-        if (w0 > 0)
-        {
-            m0 /= w0;
-        }
-        if (w1 > 0)
-        {
-            m1 /= w1;
-        }
+        m0 = (w0 > 0) ? (m0 / w0) : 0;
+        m1 = (w1 > 0) ? (m1 / w1) : 0;
 
         // if inter-class variance is maximum, current threshold is the best
-        int64_t d = w0 * w1;
-        int64_t n = (w0 + w1) * (w0 + w1);
-        long double icvar = ((long double)d / n) * (m0 - m1) * (m0 - m1);
+        // need to consider overflow
+        // e.g.) 256x256: (w0+w1)=65536, 655360^2=4.3x10^9 > UINT32_MAX (2^32-1)
+        int64_t w_prod = w0 * w1;
+        int64_t n_sq = (int64_t)(w0 + w1) * (w0 + w1);
+        double m_diff = m0 - m1;
+        double icvar = ((double)w_prod / n_sq) * m_diff * m_diff;
         if (icvar > max_icvar)
         {
             max_icvar = icvar;
@@ -68,17 +84,14 @@ Imgdata *binarize_otsu(Imgdata *img)
     printf("threshold >> %d\n", best_th);
 
     // binarization
-    for (int y = 0; y < img->height; y++)
+    Imgdata *bin = Imgdata_alloc(img->width, img->height, 1);
+    for (int i = 0; i < size; i++)
     {
-        for (int x = 0; x < img->width; x++)
-        {
-            uint8_t r = Imgdata_at(img, x, y)[0];
-            uint8_t g = Imgdata_at(img, x, y)[1];
-            uint8_t b = Imgdata_at(img, x, y)[2];
-            uint8_t gray = (uint8_t)(0.2126 * r + 0.7152 * g + 0.0722 * b);
-            Imgdata_at(bin, x, y)[0] = ((gray <= best_th) ? 0 : 255);
-        }
+        uint8_t p = gray->data[i];
+        bin->data[i] = ((p <= best_th) ? 0 : 255);
     }
+
+    Imgdata_free(gray);
 
     return bin;
 }
