293e248e49b7a1607862c8f11d69639274f8fbf9
mts0629 <soratobi93@gmail.com>
2023-01-14 23:52:08 +0900

    Add q20

---
+++ answers/020_histogram.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "imgdata.h"
+
+void get_histogram(Imgdata *img, int *histogram, const int num_bins)
+{
+    if ((num_bins > (UINT8_MAX + 1)) || (num_bins < 1)) {
+        return;
+    }
+
+    // Initialize histogram
+    for (int i = 0; i < num_bins; i++) {
+        histogram[i] = 0;
+    }
+
+    // Calculate the step of histogram
+    double step = (double)(UINT8_MAX + 1) / num_bins;
+
+    // Count bins
+    const int size = img->width * img->height * img->channel;
+    for (int i = 0; i < size; i++) {
+        int bin = (int)(img->data[i] / step);
+        histogram[bin]++;
+    }
+}
+
+#define NUM_BINS 256
+
+int main(int argc, char *argv[])
+{
+    Imgdata *img = Imgdata_read_png(argv[1]);
+
+    int histogram[NUM_BINS];
+
+    get_histogram(img, histogram, NUM_BINS);
+
+    // Write a histogram as CSV file
+    FILE *fp = fopen("./020_hist.csv", "w");
+    for (int i = 0; i < NUM_BINS; i++) {
+        fprintf(fp, "%d,%u\n", i, histogram[i]);
+    }
+    fclose(fp);
+
+    Imgdata_free(&img);
+
+    return 0;
+}
